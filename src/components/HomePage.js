7d17f604f2e5ced211126ca95c08bf9adb0d529f
jared-v-samonte <jared.v.samonte@gmail.com>
2022-02-12 08:13:12 -0600

    Homepage
    Homepage is Updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,33 +5,42 @@ import {Button, View} from 'react-native';
 
 class HomePage extends Component {
 
+  
+
   render() {
     return (
+      
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
   
       <h1>InterPlanetary 721s</h1>
       
+      <div class="marginButton">
       <Button
       title="Change to Images"
       onPress={() => this.props.navigation.navigate('Image')}
       />
-
-
-      <div >Currently functioning in the Goerli test network. </div>
-
-      <div class="container">
-      <p>
-        Uses :
-        * Hardhat instead of Truffle
-        * Ether.js instead of Web3.js
-        * OpenZeppelin
-        * OpenSea API
-        * Goerli
-      </p>
       </div>
-      <a href="https://www.linkedin.com/in/jared-samonte-9b9192157/">LinkedIn Account</a>
-      <a href="https://github.com/jared-v-samonte/ERC721-with-Hardhat">GitHub Repository</a>
 
+      
+      <div class="container" >
+      <div class="marginTitle"> This website uses the following (works only on in the Goerli testnetwork for the moment):</div>
+        <ul>
+          <li>Hardhat instead of Truffle</li>
+          <li>Ether.js instead of Web3.js</li>
+          <li>IPFS (Inter Planetary File System)</li>
+          <li>ERC721 contracts</li>
+          <li>OpenZeppelin</li>
+          <li>OpenSea API</li>
+        </ul>
+      </div>
+      <div class="container" >
+      <div class="marginTitle"> Imporant Links:</div>
+
+        <ul>
+          <li><a href="https://www.linkedin.com/in/jared-samonte-9b9192157/">LinkedIn Account</a></li>
+          <li><a href="https://github.com/jared-v-samonte/InterPlan721">GitHub Repository</a></li>
+        </ul>
+      </div>
 
       </View>
       
